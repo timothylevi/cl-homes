377b939f7c3e6144e02d04b901004a1e53c304b6
paris <paris.yee@gmail.com>
2014-02-21 15:44:10 -0500

    Link to post show on click of marker on map view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,18 @@ class CategoriesController < ApplicationController
   def map
     @category = Category.find(params[:id])
     @posts = @category.posts
+    
+    klass_name = @category.name
+    if klass_name == "housing"
+      @urls = @posts.map do |post|
+        housing_post_url(post)
+      end
+    else
+      @urls = @posts.map do |post|
+        sale_post_url(post)
+      end
+    end
+
     render layout: "map_layout"
   end
   
