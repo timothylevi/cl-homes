943624ab026b32a8e84b62b282d011376bd568a8
paris <paris.yee@gmail.com>
2014-02-28 15:17:59 -0500

    heroku ugh

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-# User.create(email: "bubblegumlovr@email.com", password: "password", fullname: "Rachel Hubert", broker: "broker",
-# company: 'The Rachel Broker Company', phone: "555 677 8888" )
+User.create(email: "bubblegumlovr@email.com", password: "password", fullname: "Rachel Hubert", broker: "broker",
+company: 'The Rachel Broker Company', phone: "555 677 8888" )
 
 
 HousingPostMaker.make_posts
